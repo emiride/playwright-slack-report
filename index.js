acbdf4ca2f8edadfbc3a75d7284f2cb920e7b18c
emiride <emir.hodzich@gmail.com>
2022-06-19 07:14:42 +0200

    hello world version

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import core from '@actions/core';
+import github from '@actions/github';
 import fetch from 'node-fetch';
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const slackWebhookUrl = core.getInput('slack-webhook-url');
-    const reposnse = await fetch(slackWebhookUrl, {
+    
+    let slackWebhookUrl = core.getInput('slack-webhook-url');
+    (async () => {
+      await fetch(slackWebhookUrl, {
         method: 'POST',
         headers: {'Content-type': 'application/json'},
         body: JSON.stringify({"text": "Hello, world."})
-    });
+      });
+    })();
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
